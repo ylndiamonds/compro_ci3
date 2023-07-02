7b3c8ef3a2fb9ecb7c3c87818d1bc16a0eff0aff
Yalon Pagewang <100259526+ylndiamonds@users.noreply.github.com>
2023-07-02 12:14:45 +0800

    Add files via upload

---
+++ data/MenaraTelepon_12.js
@@ -0,0 +1,1 @@
+var json_MenaraTelepon_12 = {"type":"FeatureCollection","name":"MenaraTelepon_12","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Label":"Menara Telepon","LCODE":"HC0140"},"geometry":{"type":"Point","coordinates":[124.283377374000111,0.738670938000041]}}]}
